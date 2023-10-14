7e9cba054b12a2db72426ad5ca9df433e882f628
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2023-10-14 16:19:02 +0400

    implemented DefaultTopAppBar which has branding and search iconButton

--- app/src/main/java/com/example/googlebooks/ui/GoogleBooksApp.kt
+++ app/src/main/java/com/example/googlebooks/ui/GoogleBooksApp.kt
@@ -2,12 +2,19 @@ package com.example.googlebooks.ui
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.googlebooks.R
 import com.example.googlebooks.ui.screens.GoogleBooksScreen
@@ -31,7 +38,35 @@ fun GoogleBooksApp() {
     }
 }
 
+@Composable
+fun GoogleBooksTopAppBar(modifier: Modifier = Modifier) {
 
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DefaultTopAppBar(
+    oneSearchClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    CenterAlignedTopAppBar(
+        title = {
+            GoogleBooksBranding()
+        },
+        actions = {
+            IconButton(
+                onClick = oneSearchClicked
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Search,
+                    contentDescription = stringResource(R.string.search_icon),
+                    tint = Color.Black
+                )
+            }
+        },
+        modifier = modifier
+    )
+}
 
 @Composable
 fun GoogleBooksBranding(
@@ -44,4 +79,12 @@ fun GoogleBooksBranding(
     )
 }
 
+
+@Preview
+@Composable
+fun DefaultTopAppBarPreview() {
+    DefaultTopAppBar(
+        oneSearchClicked = {}
+    )
+}
 
